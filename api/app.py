4e237258256df63cb6afed9bb85f86280d2e2ea7
Divkov575 <dmitriy@ivkov.net>
2025-07-22 14:49:36 -0400

    attempting to convert to single file dist - terrible failing

--- api/app.py
+++ api/app.py
@@ -4,12 +4,13 @@ import os
 import traceback
 import time
 from typing import Mapping, Any, Dict
+import logging
 
 from flask import Flask, request, send_from_directory, render_template
 import werkzeug.datastructures
 
 import pydicom
-import pydicom.uid
+# import pydicom.uid
 
 import api.utils
 from api import logging_utils
@@ -24,14 +25,16 @@ class Args(object):
 
 
 def set_model(config: Dict[str, Any]):
-    model_name = config['MODEL_NAME']
+    model_name = "mirai"
     model_args = config['MODEL_ARGS']
     model_args = Args(model_args)
+
+
 
     if model_name in model_dict:
         config['MODEL'] = model_dict[model_name](model_args)
     else:
-        raise KeyError("Model '{}' not found in model dictionary".format(model_name))
+        raise KeyError("Model '{}' not found in model dictionary: {}".format(model_name, model_dict))
 
 def _parse_multipart(response):
     """
